717cdd28c3edc2a894c71f7b5549b989a8a256ab
aaronykng <aaronykng@gmail.com>
2016-12-10 19:23:08 -0800

    Carthage

---
+++ UIViewBordersSwift/UIViewBordersSwift.h
@@ -0,0 +1,19 @@
+//
+//  UIViewBordersSwift.h
+//  UIViewBordersSwift
+//
+//  Created by Aaron Ng on 12/10/16.
+//  Copyright © 2016 Aaron Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIViewBordersSwift.
+FOUNDATION_EXPORT double UIViewBordersSwiftVersionNumber;
+
+//! Project version string for UIViewBordersSwift.
+FOUNDATION_EXPORT const unsigned char UIViewBordersSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewBordersSwift/PublicHeader.h>
+
+
